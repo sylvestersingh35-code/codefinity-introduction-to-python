4c083ef7f6866dde05cad9ca98702003325b9e43
sylvestersingh35-code <>
2026-03-03 15:56:01 +0000

    Built-in Functions

--- functions/built_in_functions/main.py
+++ functions/built_in_functions/main.py
@@ -6,3 +6,15 @@ products = {
     "Mango": ["1.75", "40"]
 }
 total_sales_list = []
+for product, valuse in products.items():
+    price = float(valuse[0])
+    quantity = int(valuse[1])
+    total_sales = price * quantity
+    print(f"Total Sales for {product}: ${total_sales}")
+    total_sales_list.append(total_sales)
+total_sum =sum(total_sales_list)
+print(f"\nTotal sum of all sales: ${total_sum}")
+min_sales = min(total_sales_list)
+max_sales = max(total_sales_list)
+print(f"Minimum sales: ${min_sales}")
+print(f"Maximum sales: ${max_sales}")
