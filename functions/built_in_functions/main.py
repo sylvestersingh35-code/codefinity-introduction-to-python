3a115d4cc5cb3280b890b35498c281aa4e412129
sylvestersingh35-code <>
2026-03-03 14:55:42 +0000

    Built-in Functions

---
+++ functions/built_in_functions/main.py
@@ -0,0 +1,8 @@
+# Dictionary of products with price and quantity sold as strings
+products = {
+    "Apple": ["1.20", "50"],   # "Item": [price, quantity sold]
+    "Banana": ["0.50", "100"],
+    "Cherry": ["2.50", "25"],
+    "Mango": ["1.75", "40"]
+}
+total_sales_list = []
